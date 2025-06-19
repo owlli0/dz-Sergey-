c015d43610f5456d5e1b4dea233dd1495883c8e1
owlli0 <balavensydbu@gmail.com>
2025-06-19 22:00:59 +0300

    remake

--- dz170625/src/TaskText2.java
+++ dz170625/src/TaskText2.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 
-public class TaskText {
+public class TaskText2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        int[][] array = new int[n][m];
+        int sum = 0;
 
+        int[][] array = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (j % 2 != 0){
-                    array[i][j] = 5;
-                }
+                array[i][j] = (int) (Math.random() * 50 + 1);
             }
         }
         for (int i = 0; i < n; i++) {
@@ -20,6 +19,14 @@ public class TaskText {
             }
             System.out.println();
         }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (j % 2 != 0){
+                    sum += array[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
 //решено
